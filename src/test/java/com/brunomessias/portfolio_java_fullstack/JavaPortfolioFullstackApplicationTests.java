765480276cbefa126dad6709edb54b7d1df61bd7
Bruno dos Santos Messias <brunodsm10@gmail.com>
2025-07-18 15:12:12 -0300

    :tada: Begin project

---
+++ src/test/java/com/brunomessias/portfolio_java_fullstack/JavaPortfolioFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomessias.portfolio_java_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPortfolioFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
